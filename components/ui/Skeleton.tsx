de559ae9f1aa722111254fda2ee2be9df8720f4d
Lucid-space <samuelosho83@gmail.com>
2026-09-05 22:48:18 +0100

    fix: add width and height props to Skeleton component to fix TypeScript errors

--- components/ui/Skeleton.tsx
+++ components/ui/Skeleton.tsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import { cn } from '@/lib/utils';
 
-interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {
+interface SkeletonProps extends Omit<React.HTMLAttributes<HTMLDivElement>, 'width' | 'height'> {
   variant?: 'text' | 'circular' | 'rectangular';
+  width?: string | number;
+  height?: string | number;
 }
 
 export const Skeleton = React.forwardRef<HTMLDivElement, SkeletonProps>(
-  ({ className, variant = 'text', ...props }, ref) => {
+  ({ className, variant = 'text', width, height, style, ...props }, ref) => {
     const variantStyles = {
       text: 'h-4 rounded-md',
       circular: 'rounded-full',
       rectangular: 'rounded-md',
     };
-    
+
+    const inlineStyle: React.CSSProperties = {
+      ...(width !== undefined && { width: typeof width === 'number' ? `${width}px` : width }),
+      ...(height !== undefined && { height: typeof height === 'number' ? `${height}px` : height }),
+      ...style,
+    };
+
     return (
       <div
         ref={ref}
@@ -21,6 +29,7 @@ export const Skeleton = React.forwardRef<HTMLDivElement, SkeletonProps>(
           variantStyles[variant],
           className
         )}
+        style={inlineStyle}
         {...props}
         aria-hidden="true"
       />
